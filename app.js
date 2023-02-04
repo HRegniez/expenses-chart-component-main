1a6c818aa451ab87709ee8aa82d1da3288e482c7
HRegniez <henriregniez124@gmail.com>
2023-02-04 16:29:34 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const response = await fetch('data.json');
 const data = await response.json();
 
-const container = document.querySelector(.main_chart);
+const container = document.querySelector(".main_chart");
 
 for (let i = 0; i < data.length; i++) {
 
@@ -9,8 +9,12 @@ for (let i = 0; i < data.length; i++) {
 
     const amount = document.createElement("div");
     amount.style = "height:" + data.amount/10 + "rem";
-    amount.classList.add
+    amount.classList.add('main_bar');
 
     const day = document.createElement("p");
     day.innerText = data.day;
+    day.classList.add('main_chart_day');
+
+    container.appendChild(amount);
+    container.appendChild(day);
 }
