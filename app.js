1e7d20d9340fc4d471661f3a2a5e771069890daa
HRegniez <henriregniez124@gmail.com>
2023-02-08 09:58:42 +0100

    update

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const response = await fetch('data.json');
-const data = await response.json();
+fetch("./data.json")
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data) {
+        console.log(data);
 
-const container = document.querySelector(".main_chart");
+        const container = document.querySelector(".main_chart");
 
-for (let i = 0; i < data.length; i++) {
+        for (let i = 0; i < data.length; i++) {
+            const days = data[i];
+            
 
-    const dayContain = document.createElement("div");
+            const dayContain = document.createElement("div");
 
-    const amount = document.createElement("div");
-    amount.style = "height:" + data.amount/10 + "rem";
-    amount.classList.add('main_bar');
+            const amount = document.createElement("div");
+            amount.style = "height:" + days.amount/10*2 + "rem";
+            amount.classList.add('main_bar');
 
-    const day = document.createElement("p");
-    day.innerText = data.day;
-    day.classList.add('main_chart_day');
+            const day = document.createElement("p");
+            day.innerText = days.day;
+            day.classList.add('main_chart_day');
 
-    container.appendChild(amount);
-    container.appendChild(day);
-}
+            dayContain.appendChild(amount);
+            dayContain.appendChild(day);
+            container.appendChild(dayContain);
+        }
+    })
+
